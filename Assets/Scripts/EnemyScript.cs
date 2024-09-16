b984b1e6497d18e9de7696dbf8c007c50c72b2d9
ALabroo18 <102484252+ALabroo18@users.noreply.github.com>
2024-09-16 15:26:52 -0400

    Update EnemyScript.cs

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -25,37 +25,43 @@ public class EnemyScript : MonoBehaviour
         //Uses distance to determine whether or not the enemy is close to the player
         var dist = Vector3.Distance(player.position, transform.position);
 
+        var minDistance = 1;
+        var maxDistance = 10;
+
         //Determines how fast the enemy will try to move close to player if they are within the same distance
         float step = 0.5f * Time.deltaTime;
         Vector3 Direction = (player.position - transform.position ).normalized;
-       /* Quaternion lookRotation = Quaternion.LookRotation(Vector3.forward, directionToPlayer);
-*/
+        Quaternion lookRotation = Quaternion.LookRotation(Vector3.forward, Direction);
+
         if (dist > 10)
         {
-            transform.RotateAround(radius.position, Vector3.forward, 60 * Time.deltaTime);
-            /*transform.position = Vector3.MoveTowards(transform.position, player.position, step);*/
+            transform.RotateAround(radius.position, Vector3.forward, 100 * Time.deltaTime);
+           
             Debug.Log("More than 10");
 
         }
         else if (dist <= 10 && dist > 5)
         {
-            transform.RotateAround(radius.position, Vector3.forward, 50 * Time.deltaTime);
-            /* transform.position = Vector3.MoveTowards(transform.position, player.position, step);*/
+            transform.RotateAround(radius.position, Vector3.forward, 70 * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, player.position, step);
 
             Debug.Log("10");
         }
         else if(dist <= 5 && dist > 1)
         {
-            transform.RotateAround(player.position, Vector3.forward, 40 * Time.deltaTime);
+            transform.RotateAround(radius.position, Vector3.forward, 40 * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, player.position, step);
             Debug.Log("5");
         }
         else if(dist <= 1)
         {
-            transform.RotateAround(player.position, Vector3.forward, 10 * Time.deltaTime);
+            transform.RotateAround(radius.position, Vector3.forward, 30 * Time.deltaTime);
+            
             Debug.Log("1");
         }
-       
-        
+        transform.position = Vector3.MoveTowards(transform.position, player.position, step);
+
+
 
     }
 }
